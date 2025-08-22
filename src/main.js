5d45f12951a182d0a1da7725dc9a7daa3e98ab8a
Henrique Lima <henrilima.contactme@gmail.com>
2025-08-22 01:59:01 -0300

    Add foundational styles for UI components and layout
    - Introduced _ui.scss for global styles.
    - Created header.scss for header component styles, including layout and link styles.
    - Established style.scss for overall application styles, including font imports, root variables, body styling, and main layout.
    - Implemented responsive design elements and animations for slider functionality.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import Aos from "aos";
+
+import "./assets/ui/style.scss";
+import "./assets/ui/_ui.scss";
+import "aos/dist/aos.css";
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { CoDevTo, BiBatteryFull } from "oh-vue-icons/icons";
+
+addIcons(CoDevTo, BiBatteryFull);
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.component("v-icon", OhVueIcon);
+
+app.mount("#app");
+
+Aos.init();
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <a><v-icon name="co-dev-to" scale="2" data-aos="fade-down" data-aos-duration="800" data-aos-delay="1600"></v-icon> Henrique Lima</a>
+      <nav>
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <div class="texts">
+        <div class="_badge" data-aos="fade-right" data-aos-duration="800">
+          <v-icon name="bi-battery-full" scale="1" class="_icon"></v-icon><p>Desenvolvedor Web Full-Stack</p>  
+        </div>
+        <h2 class="_title" data-aos="fade-left" data-aos-duration="800" data-aos-delay="400">Aplicações Modernas, do Front-end ao Back-end</h2>
+        <p class="_p" data-aos="fade-right" data-aos-duration="800" data-aos-delay="800">Transformo conceitos em realidade digital utilizando o ecossistema JavaScript. Sou proficiente em criar experiências de usuário dinâmicas com Vue.js e em construir APIs eficientes e escaláveis com Node.js, entregando produtos completos e modernos.</p>
+      </div>
+      <img :src="me" class="me" :draggable="false" data-aos="fade-left" data-aos-duration="800" data-aos-delay="1200">
+    </main>
+
+    <div class="slider" style="
+      --width: 280px;
+      --height: 56px;
+      --quantity: 6;
+    ">
+      <div class="list">
+        <div class="item" style="--position: 1;"><p>Estudante de EGS</p></div>
+        <div class="item" style="--position: 2;"><p>Ecossistema JavaScript</p></div>
+        <div class="item" style="--position: 3;"><p>Soluções Escaláveis</p></div>
+        <div class="item" style="--position: 4;"><p>Estudante de EGS</p></div>
+        <div class="item" style="--position: 5;"><p>Ecossistema JavaScript</p></div>
+        <div class="item" style="--position: 6;"><p>Soluções Escaláveis</p></div>
+      </div>
+    </div>
+
+    <section>
+    </section>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import me from "@/assets/image/me.png";
 </script>
